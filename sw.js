b28f0f656d0e55d6fac25cef55faf97ba7bcc444
midiaatos <midiaatos2023@gmail.com>
2026-08-08 07:44:10 -0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'midia-atos-v1';
+const urlsToCache = [
+  '/escalamidia/',
+  '/escalamidia/index.html'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    fetch(event.request).catch(function() {
+      return caches.match(event.request);
+    })
+  );
+});
